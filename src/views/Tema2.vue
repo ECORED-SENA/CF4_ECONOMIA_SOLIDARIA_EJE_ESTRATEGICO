b61740665c74975079aaa4d5acd269787bee5cef
Andrea Botello <andrea.botellor@misena.edu.co>
2022-03-11 19:16:37 -0500

    tema 2

--- src/views/Tema2.vue
+++ src/views/Tema2.vue
@@ -3,7 +3,136 @@
   BannerInterno
   .container.tarjeta.tarjeta--blanca.p-4.p-md-5
     
-   
+    .titulo-principal.color-primario
+      .titulo-principal__numero.text-white
+        span 2
+      h1 Fondos monetarios y políticas de inversión de excedentes
+    
+    p(data-aos="fade") Para abordar los fondos monetarios establecidos por la norma para las empresas de la economía solidaria, primero se abordan conceptos clave como los activos de un balance o estado de situación financiera de las entidades del sector solidario, los cuales están conformados por las cuentas que representan los bienes, derechos y pertenencias, tangibles e intangibles, con los que se espera obtener en el futuro beneficios económicos (Cardozo, 2015).
+
+    p.mb-5(data-aos="fade") A continuación, se explican estos conceptos clave.
+
+
+    ImagenInfograficaB.color-primario.tema-2-modal.mb-5
+      template(v-slot:imagen)
+        figure
+          img(src='@/assets/curso/tema2-infog1.svg', alt='Conceptos clave')
+
+      div(x="29%" y="8.5%")
+        h3.mb-2 Registros de aportes
+
+
+
+
+    .row.tema1-rectangulo.justify-content-center.align-items-center.mb-5(data-aos="fade-down")
+      .col-md-2.contenedor-imagenes
+        img(src='@/assets/curso/tema2-icono.svg' alt='Imagen decorativa' style="width: 135px")
+      .col-md-8.py-5.py-md-2
+        .row
+         .col-md-11
+            p.px-md-3.mb-0 Es de aclarar que el manejo contable de las empresas de economía solidaria está sujeto a la legislación cooperativa vigente, sus decretos y a las Normas Internacionales de Información Financiera; así como al catálogo de cuentas que utilizan las empresas del sector solidario.
+
+    p(data-aos="fade") Con la Ley 1314 de 2019 se regulan los principios y normas de contabilidad e información financiera y de aseguramiento de información aceptados en Colombia, así mismo, se determina que con observancia de los principios de equidad, reciprocidad y conveniencia nacional, las personas jurídicas y naturales obligadas a llevar la contabilidad realicen la convergencia de las normas contables con estándares internacionales que permita la facilidad y conveniencia de hacer negocios a escala global, sin que existan barreras contables y financieras que impidan la transacción. 
+
+    p.mb-5(data-aos="fade") Entre los objetivos de la implementación de las NIIF (Normas Internacionales de Información Financiera) regidos para el sector solidario se encuentran los siguientes:
+
+    .row.mb-5(data-aos="fade-down")
+      h3.text-center.mb-4 Objetivos de las NIFF
+      figure
+        img(src='@/assets/curso/tema2-infog2.svg', alt='Objetivos de las NIFF')
+
+    p(data-aos="fade") En consecuencia, para una adecuada gestión contable y financiera de la empresa de economía solidaria se requiere una asesoría permanente del profesional de la contaduría pública que garantice el cumplimiento oportuno de la normativa contable establecida en la Ley 1314 de 2009. De otra parte, hay que mencionar que la Supersolidaria emitió la Circular Básica Contable y Financiera Externa 22 de 2020, que entró en vigencia con la publicación en el Diario Oficial 51.570 del 27 de enero de 2021. 
+
+    p(data-aos="fade") En esta circular, existen algunas consideraciones en su capítulo IV para el manejo, registro y medición de los fondos sociales y fondos mutuales que se refieren a los recursos con una destinación específica y son la base fundamental de las cooperativas, fondos de empleados y asociaciones mutuales (Supersolidaria, s. f.).
+
+    p.mb-5(data-aos="fade") Estos fondos se dividen en tres grupos, tal como se presenta a continuación:
+
+    .row.justify-content-center.mb-5(data-aos="fade-down")
+      .col-md-10.mb-md-5
+        h3.text-center.mb-4 Tipos de fondos
+        figure
+          img(src='@/assets/curso/tema2-infog3.svg', alt='Tipos de fondos')
+
+    .row.justify-content-center.mb-5
+      h3.mb-5 Fondo social pasivo
+      .col-md-10
+        .row.d-flex
+          .col-md-5.bg-secundario.text-white.img-center.p-4.mb-4.mb-md-0(data-aos="fade-right")
+            img.mb-2.centrar-img(src='@/assets/curso/tema2-ico1.svg', alt='Icono', style="width: 40px")
+            h3.text-center Fondos sociales, según la Ley 79 de 1988
+          .col-md-7.bg-5.p-4(data-aos="fade-left")
+            p.mb-0 Los fondos sociales pasivos hacen referencia exclusiva a los fondos de solidaridad y de educación, cuyas entradas económicas se originan de los excedentes del ejercicio contable, es decir de las utilidades que genera el desarrollo de la actividad económica pero que, en empresas de economía solidaria, sin ánimo de lucro, se denominan excedentes, y estos excedentes económicos en cuanto a su destino y uso están determinados por la ley.
+
+    p.mb-5(data-aos="fade") La ley especifica el uso porcentual de los excedentes económicos de la empresa solidaria, que se verán más adelante y sus políticas para el destino, manejo y uso
+
+    h3(data-aos="fade") Fondo de educación
+
+    p(data-aos="fade") Es creado para que los recursos económicos colocados en este, su destino y uso sean para el fortalecimiento del principio cooperativo número siete, que refiere en la Ley 454 de 1998, así: formación e información para sus miembros, de manera permanente, oportuna y progresiva.
+
+    p.mb-5(data-aos="fade") Se hace énfasis en la educación en economía solidaria en temas como la educación cooperativa, las responsabilidades y naturaleza de cada una de las formas jurídicas, la contabilidad, la gestión administrativa y estratégica; así como, la formación relacionada con la asistencia técnica, de investigación y de promoción del cooperativismo de las empresas de economía solidaria.
+
+    .row.justify-content-center.mb-5
+      .col-md-12
+        .row.hover-columna.banner
+          .col-md-8
+            img(src='@/assets/curso/tema2-banner.png' alt='Imagen decorativa')
+          .col-md-6.tarjeta.bg-acento-botones.float-right
+            .row.justify-content-center
+              .col-md-11.p-4
+                p.mb-0.text-white Los fondos de educación también podrán ser creados por las asociaciones mutuales; así mismo, los fondos de empleados podrán un fondo de educación, de acuerdo con el numeral 2 del artículo 19 del Decreto Ley 1481 de 1989, que menciona la obligatoriedad de adelantar programas y actividades relacionadas con la participación democrática y el desempeño idóneo en la gestión empresarial. (Supersolidaria, 2021).
+
+    p(data-aos="fade") Los excedentes económicos de la empresa solidaria se podrán destinar al fondo de educación, cumpliendo los procedimientos técnicos y normativos contables, así como las disposiciones realizadas por la asamblea general reflejadas en el plan de acción preparado por la empresa solidaria.
+
+    p.mb-5(data-aos="fade") La circular básica contable y financiera establece los pagos que se pueden registrar con cargo al fondo de educación. A continuación, se exponen cada uno de ellos.
+
+    TabsA.color-secundario.mb-5
+      .row.justify-content-center.align-items-center.tarjeta.bg-3.p-4(titulo="1. Formación académica")
+        .col-md-7
+          h4 1. Formación académica
+          p Formación académica relacionada con cursos presenciales o a distancia, conferencias, mesas redondas, paneles, seminarios, talleres y demás eventos colectivos que tengan por objetivo predominante la formación o capacitación teórica y práctica de los asociados que asistan a dichos eventos.
+        .col-md-4
+          img.p-md-3(src='@/assets/curso/tema2-tab1.svg' alt='Imagen decorativa')
+      .row.justify-content-around.align-items-center.tarjeta.bg-3.p-4(titulo="2. Información")
+        .col-md-7
+          h4 2. Información 
+          p Elaboración o compra de folletos, cartillas, libros, boletines, carteleras y demás publicaciones impresas que tengan por objetivo la formación, información y capacitación de sus lectores.
+        .col-md-3
+          img.p-md-3(src='@/assets/curso/tema2-tab2.svg' alt='Imagen decorativa')  
+      .row.justify-content-around.align-items-center.tarjeta.bg-3.p-4(titulo="3. Capacitación")
+        .col-md-7
+          h4 3. Capacitación 
+          p Elaboración o compra de medios audiovisuales, cuyos contenidos tengan por objetivo la formación y capacitación de sus usuarios y la creación de páginas web destinadas a la capacitación sobre el tema de educación.
+        .col-md-3
+          img.p-md-3(src='@/assets/curso/tema2-tab3.svg' alt='Imagen decorativa')
+      .row.justify-content-around.align-items-center.tarjeta.bg-3.p-4(titulo="4. Investigación")
+        .col-md-7
+          h4 4. Investigación 
+          p Investigaciones efectuadas con medios técnicos y científicos y personal idóneo que permitan el desarrollo de los fines educativos estatutarios consagrados por la organización solidaria o que contribuyan a su actividad económica, siempre que se ajusten a los principios y filosofía del sector solidario.
+        .col-md-3
+          img.p-md-3(src='@/assets/curso/tema2-tab4.svg' alt='Imagen decorativa')
+      .row.justify-content-center.align-items-center.tarjeta.bg-3.p-4(titulo="5. Bienes muebles")
+        .col-md-7
+          h4 5. Bienes muebles 
+          p Adquisición de bienes muebles que tengan por objeto principal, dotar a las organizaciones solidarias de los medios o instalaciones adecuadas para adelantar las actividades de educación. 
+          p En este caso, el presupuesto debe diferenciar claramente las partidas que implican gasto, de las que constituyen inversión, existiendo un sano equilibrio entre las mismas, de acuerdo con las necesidades particulares de cada organización y el monto de los recursos destinados a la educación.
+        .col-md-4
+          img(src='@/assets/curso/tema2-tab5.svg' alt='Imagen decorativa')
+      .row.justify-content-center.align-items-center.tarjeta.bg-3.p-4(titulo="6. Gastos y costos")
+        .col-md-7
+          h4 6. Gastos y costos 
+          p Se pueden sufragar aquellos costos y gastos indirectos que surgen, en virtud de la realización de los eventos educativos, como: gastos de viaje, hospedaje, conferencistas, refrigerios, materiales de apoyo consumibles, transportes terrestres o aéreos, alquiler de auditorios y su logística.
+        .col-md-4
+          img(src='@/assets/curso/tema2-tab6.svg' alt='Imagen decorativa')                
+
+    .row.tema1-rectangulo.justify-content-center.align-items-center.mb-5(data-aos="fade-down")
+      .col-md-2.contenedor-imagenes
+        img(src='@/assets/curso/tema2-icono2.svg' alt='Imagen decorativa' style="width: 135px")
+      .col-md-8.py-5.py-md-2
+        .row
+         .col-md-11
+            p.px-md-3.mb-0 En conclusión, aquellas actividades directas e indirectas relacionadas con el objetivo del fondo de educación se causarán en las respectivas cuentas contables a cargo de este rubro, estas operaciones deben estar autorizadas por la asamblea general o en su defecto el consejo de administración.
+
+
 
 </template>
 
