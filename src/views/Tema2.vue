3a1452f7d2e5703c3c954b812dfc420eb6491885
Andrea Botello <andrea.botellor@misena.edu.co>
2022-03-11 20:15:02 -0500

    tema 2

--- src/views/Tema2.vue
+++ src/views/Tema2.vue
@@ -18,10 +18,98 @@
         figure
           img(src='@/assets/curso/tema2-infog1.svg', alt='Conceptos clave')
 
-      div(x="29%" y="8.5%")
+      div(x="64%" y="22.5%")
         h3.mb-2 Registros de aportes
 
+      div.row.justify-content-center(x="64%" y="36.5%")
+        .col-md-11
+          h3.mb-2 Pasivo
+          p Un pasivo son las obligaciones presentes en la entidad surgidas a raíz de sucesos pasados, a cuyo vencimiento, y para pagarlas, la entidad espera desprenderse de recursos que conllevan beneficios económicos. En este grupo de cuentas se registran los rubros que representan obligaciones contraídas por la entidad en el desarrollo del giro ordinario de su actividad, sea financiera, comercio, producción o de servicios (Cardozo, 2015).  
+        .col-md-7
+          p Entre los grupos de cuentas principales para la clase de pasivo, se encuentran:
+          ul.lista-ul.px-3
+            li.mb-0
+              i.fas.fa-circle
+              | Instrumentos financieros a costo amortizado.
+            li.mb-0
+              i.fas.fa-circle
+              | Instrumentos financieros a valor razonable.
+            li.mb-0 
+              i.fas.fa-circle
+              | Aportaciones y participaciones.
+            li.mb-0 
+              i.fas.fa-circle
+              | Cuentas por pagar.
+            li.mb-0 
+              i.fas.fa-circle
+              | Fondos sociales y mutuales.
+            li.mb-0 
+              i.fas.fa-circle
+              | Obligaciones laborales por beneficios a empleados.
+            li.mb-0 
+              i.fas.fa-circle
+              | Provisiones.
+            li 
+              i.fas.fa-circle
+              | Otros pasivos.              
+        .col-md-4
+          img(src='@/assets/curso/tema2-modal-img2.svg', alt='Imagen decorativa')
 
+      div.row.justify-content-center(x="64%" y="49.5%")
+        .col-md-11
+          h3.mb-2 Patrimonio
+          p Es la participación residual en los activos de una entidad una vez deducidos todos sus pasivos. El patrimonio de las entidades del sector solidario está constituido por las reservas, los fondos de reservas, fondos de destinación específica y el resultado acumulado que son de carácter permanente, pero algunas de estas son aplicables de acuerdo con su destino (Cardozo, 2015), tales como: 
+        .col-md-7
+          ul.lista-ul.px-3
+            li.mb-0
+              i.fas.fa-circle
+              | Reserva para protección de aportes sociales.
+            li.mb-0
+              i.fas.fa-circle
+              | Fondo de amortización de aportes.
+          p Entre los grupos de cuentas se encuentran: 
+          ul.lista-ul.px-3
+            li.mb-0
+              i.fas.fa-circle
+              | Capital social.
+            li.mb-0
+              i.fas.fa-circle
+              | Reservas.
+            li.mb-0
+              i.fas.fa-circle
+              | Fondos de reserva de destinación específica.    
+        .col-md-4
+          img(src='@/assets/curso/tema2-modal-img3.svg', alt='Imagen decorativa')
+
+      div.row.justify-content-center(x="64%" y="63%")
+        .col-md-7
+          h3.mb-2 Ingresos
+          p Son los incrementos en los beneficios económicos producidos a lo largo del periodo sobre el que se informa, en forma de entradas o incrementos el valor de los activos, o la disminución de las obligaciones, que dan como resultado aumentos del patrimonio y no están relacionados con los aportes de los socios (Cardozo, 2015).
+          p El grupo principal para el registro de los ingresos provenientes del desarrollo de las actividades económicas es: 
+          ul.lista-ul.px-3
+            li.mb-0
+              i.fas.fa-circle
+              | Ingresos de actividades ordinarias generales.
+        .col-md-4
+          img.px-md-3(src='@/assets/curso/tema2-modal-img4.svg', alt='Imagen decorativa')
+
+      div.row.justify-content-center(x="64%" y="77%")
+        .col-md-7
+          h3.mb-2 Gastos y costos
+          p Son todos aquellos pagos realizados por la empresa de economía solidaria y que se generan en el normal desarrollo de las actividades productivas. Estos gastos se agrupan en administrativos, de ventas y otros gastos. 
+          p Con base en Cardozo (2015), los principales grupos de cuentas son: 
+          ul.lista-ul.px-3
+            li.mb-0
+              i.fas.fa-circle
+              | Gastos de operación.
+            li.mb-0
+              i.fas.fa-circle
+              | Costos de ventas y de prestación de servicios.
+            li.mb-0
+              i.fas.fa-circle
+              | Costos de producción.    
+        .col-md-4
+          img.px-md-3(src='@/assets/curso/tema2-modal-img5.svg', alt='Imagen decorativa')
 
 
     .row.tema1-rectangulo.justify-content-center.align-items-center.mb-5(data-aos="fade-down")
@@ -131,6 +219,94 @@
         .row
          .col-md-11
             p.px-md-3.mb-0 En conclusión, aquellas actividades directas e indirectas relacionadas con el objetivo del fondo de educación se causarán en las respectivas cuentas contables a cargo de este rubro, estas operaciones deben estar autorizadas por la asamblea general o en su defecto el consejo de administración.
+
+    h3(data-aos="fade") Fondo de solidaridad
+
+    p(data-aos="fade") Es un fondo destinado a atender eventos de solidaridad que sean considerados en el estatuto de la empresa de economía solidaria. El fin de este fondo es la ayuda mutua entre los asociados en caso de calamidad doméstica o cualquier otro evento que le suceda al asociado y que requiera de la ayuda de los miembros de la empresa de economía solidaria. 
+
+    p(data-aos="fade") Así mismo, los fondos de empleados y las asociaciones mutuales podrán crear también los fondos de solidaridad, de acuerdo con el artículo 19 del Decreto Ley 1481 de 1989 y cumpliendo los objetivos fijados en los estatutos de la organización de economía solidaria.
+
+    p.mb-5(data-aos="fade") La circular básica contable y financiera establece los pagos que se pueden registrar con cargo al fondo de solidaridad. En la siguiente figura se exponen cada uno de ellos.
+
+    .row.align-items-center.mb-5
+      h4.text-center Pagos para el fondo de solidaridad
+      .col-md-5(data-aos="fade-right")
+        figure
+          img(src='@/assets/curso/tema2-infog4.svg', alt='Pagos para el fondo de solidaridad')
+      .col-md-7(data-aos="fade-left")
+        .row.align-items-center.mb-3
+          .col-md-1
+            h2.mb-0.text-center(style="color: #00A3BE; font-size: 45px; font-weight: bold;") A
+          .col-md-10  
+            p.mb-0 Auxilio a los asociados de la cooperativa en caso de calamidad doméstica.
+        .row.align-items-center.mb-3
+          .col-md-1
+            h2.mb-0.text-center(style="color: #32D9CB; font-size: 45px; font-weight: bold;") B
+          .col-md-10  
+            p.mb-0 Auxilio para servicios médicos, hospitalarios y quirúrgicos al asociado o sus familiares en el parentesco señalado en los estatutos o reglamentos de la organización solidaria.
+        .row.align-items-center.mb-3
+          .col-md-1
+            h2.mb-0.text-center(style="color: #12263F; font-size: 45px; font-weight: bold;") C
+          .col-md-10  
+            p.mb-0 Adquisición de mausoleos, osarios o lotes en parques cementerios, destinados a facilitarles a los asociados o trabajadores mediante diversas modalidades.
+        .row.align-items-center.mb-3
+          .col-md-1
+            h2.mb-0.text-center(style="color: #727997; font-size: 45px; font-weight: bold;") D
+          .col-md-10  
+            p.mb-0 Pago total o parcial de planes de protección de servicios médicos prepagados, servicios funerarios y de exequias o servicios médicos y hospitalarios, así como demás protecciones similares
+        .row.align-items-center.mb-3
+          .col-md-1
+            h2.mb-0.text-center(style="color: #678589; font-size: 45px; font-weight: bold;") E
+          .col-md-10  
+            p.mb-0 Contribuciones y obras para lograr un desarrollo sostenible del medioambiente y de la comunidad, de conformidad con las políticas generales aprobadas por el órgano competente de la organización solidaria.
+        .row.align-items-center
+          .col-md-1
+            h2.mb-0.text-center(style="color: #F37F4C; font-size: 45px; font-weight: bold;") F
+          .col-md-10  
+            p.mb-0 Donaciones esporádicas y ocasionales frente a calamidades de sus trabajadores o a hechos que generen catástrofes o perjuicios colectivos en el entorno de la organización solidaria.            
+
+    .row.justify-content-center.mb-5(data-aos="fade-down")
+      .col-md-10.tarjeta.bg-acento-botones.p-4.px-md-5
+        p.text-white.mb-0 Todos los pagos realizados a cargo del fondo de solidaridad deberán cumplir con los procedimientos contables exigidos para el registro de los hechos económicos, así como sus respectivos soportes que respalden la transacción económica. Las actividades del fondo de solidaridad deberán ser autorizadas y reglamentadas por el consejo de administración de la empresa de economía solidaria. 
+
+    p(data-aos="fade") En síntesis, se puede decir que, aunque la reglamentación para las empresas del sector solidario determina los fondos sociales pasivos clasificados en los fondos de educación y de solidaridad, el máximo órgano de administración de la empresa solidaria puede crear otros fondos, siempre que conservan los principios y fines solidarios reglamentados por la legislación.
+
+    p.mb-5(data-aos="fade") Adicional a ello, hay que considerar las políticas para el manejo de excedentes económicos, así: 
+
+    .row.justify-content-center.mb-5(data-aos="fade-down")
+      h3.mb-5 Manejo de excedentes
+      .col-md-10  
+        figure
+          img(src='@/assets/curso/tema2-infog5.svg', alt='Manejo de excedentes')
+
+    p.mb-5(data-aos="fade") De tal forma, que el uso y destino de los excedentes económicos debe cumplir estrictamente lo establecido en la legislación, dejando abierta la posibilidad de crear nuevos fondos para servicios en beneficio del bien común o para el disfrute de la seguridad social; esta decisión se realiza de manera voluntaria por cada asociado y debe estar aprobada por la asamblea general como máximo órgano de administración de la empresa de economía solidaria. Estas políticas se deben cumplir tal y como se observó en la tabla anterior.
+
+
+    h4.mb-4(data-aos="fade") Reservas y otros fondos con fines determinados
+
+    p.mb-5(data-aos="fade") Las reservas y otros fondos pueden ser creados por las cooperativas y precooperativas amparados en la Ley 79 de 1988 como resultado de los excedentes económicos de su ejercicio contable. A continuación, se presenta una breve explicación de estos.
+
+    .row.justify-content-center.mb-5.tema-2(data-aos="fade-down")
+      .col-md-10  
+        PasosA.color-secundario(tipo="n")
+          .row.align-items-center.justify-content-around
+            .col-md-2.mb-4.mb-md-0
+              img(src='@/assets/curso/tema2-paso1.svg', alt='Icono decorativo')
+            .col-md-9
+              p.mb-0 Las reservas son de carácter patrimonial, al contrario de los fondos que pueden ser de tipo patrimonial o pasivos de carácter agotable. Tanto los fondos como las reservas deben tener un reglamento donde se especifique el destino, manejo y uso de los excedentes; estos, deben estar autorizados por la asamblea general. 
+          .row.align-items-center.justify-content-around
+            .col-md-9.mb-4.mb-md-0
+              p.mb-0 En cuanto a los procedimientos contables estos deben cumplir con las políticas y normativa establecidos por la Ley 1314 de 2009, referentes al marco normativo aplicable a MiPymes del grupo 3 relacionadas con las Normas Internacionales de Información Financiera - NIIF. De tal forma que la empresa solidaria, a partir de las necesidades y expectativas de sus asociados, está autorizada para crear, manejar e invertir sus excedentes económicos en los fondos sociales pasivos, siempre y cuando se cumplan con la legislación y normativa vigente aplicable para las empresas del sector solidario.               
+            .col-md-2
+              img(src='@/assets/curso/tema2-paso2.svg', alt='Icono decorativo')
+          .row.align-items-center.justify-content-around
+            .col-md-2.mb-4.mb-md-0
+              img(src='@/assets/curso/tema2-paso3.svg', alt='Icono decorativo')
+            .col-md-9
+              p.mb-0 Finalmente, la gestión administrativa de la empresa solidaria, el representante legal, junto con el consejo de administración y junta de vigilancia son las encargadas de que las actas de la asamblea, donde se crean y aprueban los fondos monetarios y políticas, para la inversión de los excedentes, sean archivadas y custodiadas en el archivo de gestión y estén disponibles cuando la Superintendencia de Economía Solidaria, así lo exija. 
+
+
+
 
 
 
